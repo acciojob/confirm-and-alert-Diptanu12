fc67092a36d6c3dc18170a4c5e36069068c96e96
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-04-27 09:48:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+var result = confirm("Do you want to proceed?");
+if (result === true) {
+alert("You clicked OK. Proceeding...");
+} else {
+alert("You clicked Cancel. Exiting...");
+}
+
